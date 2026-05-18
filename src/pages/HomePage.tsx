e99c21ba99aedd7de5963cd9701a06655b3f1f47
Ragavan <ragavan212005@gmail.com>
2026-05-18 20:31:12 +0530

    Remove unused import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { apiService } from '../services/api';
 
 interface Airport {
   iataCode: string;
